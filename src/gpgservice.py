b716843000f6a6310de30cebadba0fcec07f6d5d
Bartosz Irzyk <bartosz.irzyk@miquido.com>
2016-10-17 10:45:33 +0200

    modified gitignore, importing public key, export public key

--- src/gpgservice.py
+++ src/gpgservice.py
@@ -7,24 +7,25 @@ class GPGService:
 	def __init__(self):
 		super(GPGService, self).__init__()
 		self.gpg = gnupg.GPG(gnupghome ='homegpg')
+
 		with open('keys.json','r') as dataFile: 
 			self.keysFp = json.load(dataFile)
 
-	def createKey(self,email):
+	def createKeys(self, domain):
 		self.inputData = self.gpg.gen_key_input(
-				name_email= email,
-				passphrase= 'password')
+				name_email = domain,
+				passphrase = 'password')
 
 		key = self.gpg.gen_key(self.inputData)
 
-		self.keysFp[email] = key.fingerprint
+		self.keysFp[domain] = key.fingerprint
 		self.updateKeysFpFile()
 
-	def encodeMessage(self,message,senderDomain):
-		return str(self.gpg.encrypt(message,senderDomain))
+	def encodeMessage(self, message, senderDomain):
+		return str(self.gpg.encrypt(message, senderDomain))
 
-	def decodeMessage(self,message):
-		decryptedData = self.gpg.decrypt(message,passphrase='password')
+	def decodeMessage(self, message):
+		decryptedData = self.gpg.decrypt(message, passphrase ='password')
 		return decryptedData.ok
 
 	def printAllKeys(self):
@@ -35,11 +36,30 @@ class GPGService:
 		pprint.pprint(privateKeys)
 
 
-	def removeKey(self,email):
-		print(str(self.gpg.delete_keys(self.keysFp[email],True)))
-		print(str(self.gpg.delete_keys(self.keysFp[email])))
-		self.keysFp.pop(email)
+	def removeKey(self, domain):
+		print(str(self.gpg.delete_keys(self.keysFp[domain], True)))
+		print(str(self.gpg.delete_keys(self.keysFp[domain])))
+		self.keysFp.pop(domain)
 		self.updateKeysFpFile()
+
+	def savePublicKeyToFile(self, domain):
+		publickKey = self.gpg.export_keys(self.keysFp[domain])
+		filePath = 'exported_keys/' + domain + '.asc'
+		with open(filePath,'w') as inputFile:
+			inputFile.write(publickKey)
+		return filePath
+
+	def importPublicKeyFromFile(self, filePath, domain):
+		keyData = open(filePath,'r').read()
+		importResult = self.gpg.import_keys(keyData)
+
+		print('imported:')
+		pprint.pprint(importResult.fingerprints)
+
+		if not importResult.fingerprints :
+			self.keysFp[domain] = importResult.fingerprints[1]
+			self.updateKeysFpFile()
+
 
 	def updateKeysFpFile(self):
 		with open('keys.json', 'w') as outfile:
--- src/testgpg.py
+++ src/testgpg.py
@@ -2,15 +2,15 @@ from gpgservice import GPGService
 
 gpgService = GPGService()
 
-# gpgService.createKey('test1@gmail.com')
+# gpgService.createKeys('test1@gmail.com')
 
-# gpgService.createKey('test2@gmail.com')
+# gpgService.createKeys('test2@gmail.com')
 
-message = gpgService.encodeMessage('testowa wiadomosc','test2@gmail.com')
+# message = gpgService.encodeMessage('testowa wiadomosc','test2@gmail.com')
 
-decryptedStatus = gpgService.decodeMessage(message)
+# decryptedStatus = gpgService.decodeMessage(message)
 
-print ('ok: ', decryptedStatus)
+# print ('ok: ', decryptedStatus)
 # print ('status: ', decrypted.status)
 # print ('stderr: ', decrypted.stderr)
 # print ('decrypted string: ', decrypted.data)
@@ -19,5 +19,9 @@ print ('ok: ', decryptedStatus)
 
 # gpgService.removeKey('test2@gmail.com')
 # gpgService.removeKey('test1@gmail.com')
+
+
+gpgService.savePublicKeyToFile('test2@gmail.com')
+# gpgService.importPublicKeyFromFile('test2@gmail.com.asc','test2@gmail.com')
 
 gpgService.printAllKeys()
