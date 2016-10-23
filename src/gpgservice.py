9dba3fea519b91be89332748b5bddcf58df89984
Bartosz Irzyk <bartosz.irzyk@miquido.com>
2016-10-23 13:10:58 +0200

    used json instead of eval

--- src/gpgservice.py
+++ src/gpgservice.py
@@ -1,5 +1,6 @@
 import gnupg  
 import re
+import json
 
 class GPGService:
 
@@ -8,25 +9,25 @@ class GPGService:
 		self.gpg.encoding = 'utf-8'
 
 	def generate_keys(self, uuid):
-		 self.gpg.gen_key(self.gpg.gen_key_input(name_email = uuid))
+		self.gpg.gen_key(self.gpg.gen_key_input(name_email = uuid))
 
 	def get_public_key(self, uuid):
 		return self.gpg.export_keys(uuid)
 
 	def add_conversation(self, uuid, public_key):
 		import_result = self.gpg.import_keys(public_key)
-		return bool(import_result.results[0]['ok'])
+		return bool(import_result.results[0]['ok'])	
 
 	def encrypt_message(self, message, uuid):
 		message_params = {}
 		message_params['data'] = message
 		message_params['fingerprint'] = self._get_fingerprint_from_key(uuid)
-		return self.gpg.encrypt(str(message_params), uuid)
+		return self.gpg.encrypt(json.dumps(message_params), uuid)
 
 	def decrypt_message(self, encrypted):
 		result = {}
 		decrypted_data = self.gpg.decrypt(str(encrypted))
-		decrypted_data = eval(str(decrypted_data))
+		decrypted_data = json.loads(str(decrypted_data))
 		result['data'] = decrypted_data['data']
 		result['uuid'] = self._get_uuid_from_key(decrypted_data['fingerprint'])
 		return result
@@ -43,9 +44,3 @@ class GPGService:
 		for key in public_keys:
 			if uuid in key['uids'][0]:
 				return key['fingerprint']
-
-
-
-
-
-
