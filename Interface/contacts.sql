ebea2f4fbcd5089c1595ea6abbc8cdbc65f4c904
Kate <kasiaa1551@gmail.com>
2016-10-29 17:14:58 +0200

    wylistowanie imion w snd_form

--- Interface/contacts.sql
+++ Interface/contacts.sql
@@ -3,8 +3,8 @@ drop table if exists contacts;
 create table contacts(
 	id	integer primary key autoincrement,
 	name	text				not null,
-	key 	text				not null
+	fingerprint 	text	
 	);
 
-insert into contacts(name, key) values ( 'kkkk', 'vnakndlaublievabul' );
-
+insert into contacts(name) values ('ania');
+insert into contacts(name) values ('adam');
--- Interface/hello.py
+++ Interface/hello.py
@@ -6,85 +6,87 @@ from flask import Flask, render_template, request, url_for, session, g, redirect
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+
+
 @app.route("/")
 def hello(name=None):
     return render_template('main.html', name=name)
 
-@app.route("/main.html")
+@app.route("/")
 def main():
-    return render_template('main.html', method=['POST'])
+    return render_template('main.html')
 
-@app.route('/contact_form.html')
+@app.route('/contact_form')
 def client():
 	return render_template('contact_form.html')
 
-@app.route('/rcv_form.html')
+@app.route('/rcv_form')
 def recive():
 	return render_template('rcv_form.html')
 
-@app.route('/snd_form.html')
-def send():
-	return render_template('snd_form.html')
 
-
-
-		#DATABASE
 app.config.update(dict(
-	DATABASE=os.path.join(app.root_path, 'contacts.db'),
-	SECRET_KEY='development key',
-	USERNAME='admin',
-	PASSWORD='default'
+    DATABASE=os.path.join(app.root_path, 'contacts.db'),
+    DEBUG=True
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
+
 def connect_db():
-#	Connects to the specific database.
-	rv = sqlite3.connect(app.config['DATABASE'])
-	rv.row_factory = sqlite3.Row
-	return rv
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
 
-#initialize database from hello.py   #nie dziala
-#def init_db():
-#	db = get_db()
-#	with app.open_resource('contacts', mode='r') as f:
-#		db.cursor().executescript(f.read())
-#	db.commit()
 
-#@app.cli.command('initdb')
-#def initdb_command():
-#Initializes the database.
-#	init_db()
-#	print 'Initialized the database.'
+def init_db():
+    """Initializes the database."""
+    db = get_db()
+    with app.open_resource('contacts.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Creates the database tables."""
+    init_db()
+    print('Initialized the database.')
+
 
 def get_db():
-#Opens a new database connection if there is none yet for the current application context.
-	if not hasattr(g, 'sqlite_db'):
-		g.sqlite_db = connect_db()
-	return g.sqlite_db
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
 
 @app.teardown_appcontext
 def close_db(error):
-#	Closes the database again at the end of the request.
-	if hasattr(g, 'sqlite_db'):
-		g.sqlite_db.close()
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 
-@app.route('/')
+
+@app.route('/snd_form')
 def show_names():
-	db = get_db()
-	cur = db.execute('select names')
-	names = cur.fetchall()
-	return render_template('snd_form.html', names=names)
+    db = get_db()
+    cur = db.execute('select name from contacts')
+    names = cur.fetchall()
+    return render_template('snd_form.html', names=names)
 
-@app.route('/add', methods=['POST'])
-def add_entry():
-	if not session.get('logged_in'):
-		abort(401)
-	db = get_db()
-	db.execute('insert into entries (title, text) values (?, ?)',
-				[request.form['title'], request.form['text']])
-	db.commit()
-	flash('New entry was successfully posted')
-	return redirect(url_for('show_entries'))
+
+
+
+
+#@app.route('/snd_form')
+#def send():
+#	return render_template('snd_form.html')
+
+
+
 
 if __name__ == "__main__":
     app.run()
